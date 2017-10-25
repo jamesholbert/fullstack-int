5bcc0eea999cf119fc7525e2adc59287fdef531b
jamesholbert <james.holbert@ecnomicmodeling.com>
2017-10-25 09:33:25 -0700

    added css and component files, App component functional

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,22 @@
-console.log("Quarter Falcon")
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {BrowserRouter, Route} from 'react-router-dom'
+
+import './main.css'
+import './normalize.css'
+import './skeleton.css'
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		
+	}
+
+	render() {
+		return (
+			<div> Half-Lion </div>
+		)
+	}
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"))
